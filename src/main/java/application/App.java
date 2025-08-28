4b2859eb27196229b4060f0ac4d3dd1e6d42a5d6
EmersonSN <emersonn956@gmail.com>
2025-08-28 15:10:49 -0300

    Add initial logic and default test

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -1,7 +1,10 @@
 package application;
 
+import controller.DataScraping;
+
 public class App {
     public static void main(String[] args) {
-
+        DataScraping ds = new DataScraping();
+        ds.scraping();
     }
 }
--- src/main/java/controller/DataScraping.java
+++ src/main/java/controller/DataScraping.java
@@ -1,5 +1,19 @@
 package controller;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class DataScraping {
 
+    public void scraping() {
+        String driverPath = DataScraping.class.getClassLoader().getResource("chromedriver.exe").getPath();
+        System.setProperty("webdriver.chrome.driver", driverPath);
+
+        ChromeOptions options = new ChromeOptions();
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://amazon.com.br");
+
+    }
 }
